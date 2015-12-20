a2521f000b314a3e47dd7e469ffb6a0d7f81e6c8
sean harre <seanharre@gmail.com>
2015-12-20 08:27:51 -0800

    latest

--- abstractCPP/main.cpp
+++ abstractCPP/main.cpp
@@ -19,12 +19,14 @@ class ca: number {
     public:
         ca() {}
         ca(int x) { u.val = x; }
+        ca(float f) { u.valf = f; }
         ~ca() { printf("CA DEST\n"); }
         static int x;
         bool isNegative();
         void setNum(int x) { u.val = x; ca::x += 1;}
         void setNum(float x) { u.valf = x; ca::x += 1;}
         int getNum() { return u.val;}
+        float getNumf() { return u.valf;}
     private:
         union {
             int val;
@@ -40,13 +42,18 @@ int ca::x = 0;
 int main()
 {
    ca x(5);
-   printf("x.x = %d val=%d negative=%d\n",x.x,x.getNum(),x.isNegative());
+   printf("x.x = %d val=%d valf=%f (neg=%d)\n",x.x,x.getNum(),x.getNumf(),x.isNegative());
    x.setNum(3);
-   printf("x.x = %d val=%d negative=%d\n",x.x,x.getNum(),x.isNegative());
+   printf("x.x = %d val=%d valf=%f (neg=%d)\n",x.x,x.getNum(),x.getNumf(),x.isNegative());
    x.setNum(-3);
-   printf("x.x = %d val=%d negative=%d\n",x.x,x.getNum(),x.isNegative());
+   printf("x.x = %d val=%d valf=%f (neg=%d)\n",x.x,x.getNum(),x.getNumf(),x.isNegative());
    x.setNum(-0.3f);
-   printf("x.x = %d val=%d negative=%d\n",x.x,x.getNum(),x.isNegative());
+   printf("x.x = %d val=%d valf=%f (neg=%d)\n",x.x,x.getNum(),x.getNumf(),x.isNegative());
+   
+   ca y(0.5f);
+   printf("y.x = %d val=%d valf=%f (neg=%d)\n",y.x,y.getNum(),y.getNumf(),y.isNegative());
    
    return 0;
 }
+
+
