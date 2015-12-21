b52a7bc7b66b4f4deaf387536afee9537461456b
sean harre <seanharre@gmail.com>
2015-12-20 21:47:03 -0800

    generalized singleton template so config class is easier to maintain, switched config to use map for key/value storage

--- singletonCPP/main.cpp
+++ singletonCPP/main.cpp
@@ -1,30 +1,82 @@
 #include <iostream>
+#include <stddef.h>
+#include <assert.h>
+// #include <vector>
+#include <map>
+#include <string>
 
 using namespace std;
 
+//----------------------------
+// generic singleton template
+template<class T>
+class singleton {
+    public:
+        static T* Instance() {
+            if (m_inst == NULL) m_inst = new T();
+            assert(m_inst != NULL);
+            return m_inst;
+        }
+    private:
+        static T* m_inst;
+        singleton();
+        ~singleton();
+        singleton(singleton const&);
+        singleton& operator=(singleton const&);
+};
+template<class T> T* singleton<T>::m_inst = NULL; 
+//----------------------------
+
 class config {
     public:
-        static config* getInstance();
-        int getVal() { return val; }
-        void setVal(int x) { val = x; }
+        string getVal(string s) { return m_map[s]; }
+        void setKeyVal(string s, string v) { m_map[s] = v; }
+        bool delKey(string s) { return m_map.erase(s); }
+        // debug
+        void dump_config() {
+            for (map<string,string>::iterator it = m_map.begin(); it != m_map.end(); ++it)
+                printf("config [%s]=[%s]\n",it->first.c_str(),it->second.c_str());
+        }
     private:
-        static config* m_inst;
-        int val;
+        map<string,string> m_map;
 };
 
-config* config::m_inst = nullptr;
-config* config::getInstance() {
-    if (m_inst == nullptr) {
-        m_inst = new config();
-    }
-    return m_inst; 
-}
+// template<typename T>
+// class queue {
+//     public:
+//         void push(T x) { mQ.push_back(x); }
+//         T pop() { 
+//             T tmp = mQ.back();
+//             mQ.pop_back();
+//             return tmp;
+//         }
+//     private:
+//         vector<T> mQ;
+// };
+
+typedef singleton<config> Config;
 
 int main()
 {
-    config::getInstance()->setVal(3);
-    printf("int val = %d\n",config::getInstance()->getVal());
+    Config::Instance()->setKeyVal("version","3");
+    printf("config version = %s\n",Config::Instance()->getVal("version").c_str());
+    Config::Instance()->dump_config();
     
+    Config::Instance()->setKeyVal("version","5");
+    printf("config version = %s\n",Config::Instance()->getVal("version").c_str());
+    Config::Instance()->dump_config();
+     
+    bool ok = Config::Instance()->delKey("version");
+    printf("config remove version = %d\n",ok);
+    Config::Instance()->dump_config();
+    
+//   queue<int> Q;
+//   Q.push(3);
+//   Q.push(5);
+//   printf("POP=%d\n",Q.pop());
+//   printf("POP=%d\n",Q.pop());
+   
    return 0;
 }
+
 
