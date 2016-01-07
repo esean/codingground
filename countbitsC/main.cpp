757a1fcbf0f28712a5cfd866e4a6f4e98211c874
sean harre <seanharre@gmail.com>
2016-01-07 09:58:18 -0800

    added usage

--- countbitsC/main.cpp
+++ countbitsC/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const char usage[] = "%s [decimal]\nCounts bits in [decimal]\n";
+
   int CountBits(unsigned int x)
   {
       int count=0;
@@ -14,6 +16,11 @@
 
 int main(int argc, char* argv[])
 {
+    if (argc == 1)
+    {
+        printf(usage,argv[0]);
+        return 1;
+    }
 	int i = atoi(argv[1]);
 	printf("0x%x = %d\n",i,CountBits(i));
 	return 0;
