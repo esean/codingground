af0441cc6cdb2688b749e6c639b8a0e651eda8be
sean harre <seanharre@gmail.com>
2016-01-08 08:41:51 -0800

    made node class into template

--- linklistCPP/main.cpp
+++ linklistCPP/main.cpp
@@ -3,16 +3,17 @@
 
 using namespace std;
 
+template<class T>
 class node {
 	public:
-		node() { value=0; pNext=NULL; }
-		node(int v) { value=v; pNext=NULL; }
+		node() { pNext=NULL; }
+		node(T v) { value=v; pNext=NULL; }
 		~node() {}
 
 		void dump() {
 			dumper(this);
 		}
-		void set_value(int val) {
+		void set_value(T val) {
 			this->value = val;
 		}
 		void add_node(node* pNode) {
@@ -21,23 +22,23 @@ class node {
 		bool is_circular();
 	private:
 		node* pNext;
-		int value;
+		T value;
 		void dumper(node* start);
 };
 
-void node::dumper(node* start) {
+template<class T> void node<T>::dumper(node<T>* start) {
 	int i = 0;
-	printf("%d:%d\n",i,start->value);
+	cout << i << ":" << start->value << endl;
 	while (start->pNext)
 	{
 		++i;
 		start = start->pNext;
-		printf("%d:%d\n",i,start->value);
+		cout << i << ":" << start->value << endl;
 	}
 }
-bool node::is_circular() {
-	node* n1 = this;
-	node* n2 = this;
+template<class T> bool node<T>::is_circular() {
+	node<T>* n1 = this;
+	node<T>* n2 = this;
 	if (!n1) return false;
 	n2 = n2->pNext;
 	while (n1) {
@@ -50,9 +51,11 @@ bool node::is_circular() {
 
 int main()
 {
-    node first(1);
-    node second(2);
-    node third(3);
+    // INT
+    //-----------------
+    node<int> first(1);
+    node<int> second(2);
+    node<int> third(3);
 
     first.add_node(&second);
     second.add_node(&third);
@@ -66,7 +69,17 @@ int main()
     // NO! - don't call
     //first.dump();
 
-    printf("Buffer is %s circular\n", first.is_circular() ? "YES" : "NOT");
+    printf("INT Buffer is %s circular\n", first.is_circular() ? "YES" : "NOT");
+
+    // CHAR
+    //-----------------
+    node<char> c1('a');
+    node<char> c2('b');
+    node<char> c3('c');
+    c1.add_node(&c2);
+    c2.add_node(&c3);
+    c1.dump();
+    printf("CHAR Buffer is %s circular\n", c1.is_circular() ? "YES" : "NOT");
 
    return 0;
 }
