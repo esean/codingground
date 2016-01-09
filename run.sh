082b41938800226bc211f08687f612faab61b645
sean harre <seanharre@gmail.com>
2016-01-09 08:46:13 -0800

    SCRIPT: auto-create expected output files

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+die() { echo "ERROR:$0:$@ !" >&2; exit 1; }
+proc() {
+	echo "$1"
+	pushd $1
+	make > README.md || die "Couldnt build $1"
+	echo "-----------------------------------" >> README.md
+	echo >> README.md
+	echo -e "$1:\nExpected output:\n" >> README.md
+	./main >> README.md
+	svn add README.md
+	rm main
+	popd
+}
+
+for i in *; do
+	[ -d $i ] && {
+		proc $i
+	}
+done
